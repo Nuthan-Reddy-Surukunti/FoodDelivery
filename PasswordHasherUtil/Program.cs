4f786631ef486be9678ef3915d42b30e7ef14444
Surukunti Nuthan Reddy <surkuntinuthanreddy@gmail.com>
2026-04-30 13:25:49 +0530

    cleanup: remove unused PasswordHasherUtil utility

--- PasswordHasherUtil/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-var passwordHasher = new PasswordHasher<IdentityUser>();
-var testUser = new IdentityUser();
-var hashedPassword = passwordHasher.HashPassword(testUser, "password123");
-Console.WriteLine(hashedPassword);
